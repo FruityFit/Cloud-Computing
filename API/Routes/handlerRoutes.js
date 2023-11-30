0847a72b81f40bcb1fa585f8f26f1cfb2cf40ac0
ddann21 <44884504+ddann21@users.noreply.github.com>
2023-11-30 12:40:04 +0800

    Update few response body

--- API/Routes/handlerRoutes.js
+++ API/Routes/handlerRoutes.js
@@ -2,6 +2,19 @@ const express = require('express');
 const { db } = require('./config');
 const router = express.Router();
 
+// Fetch list of fruit
+router.get('/fruits', (req, res) => {
+    const sql = 'SELECT fruit_id, name, imageurl FROM fruits';
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.error('Error retrieving user information from MySQL:', err);
+        res.status(500).send('Internal Server Error');
+      } else {
+          res.json(results);
+      }
+    });
+});
+
 // Fetch fruit's information 
 router.get('/fruits/:fruitId', (req, res) => {
     const fruitId = req.params.fruitId;
@@ -32,7 +45,12 @@ router.post('/add-fav/:userId', (req, res) => {
         console.error('Error adding product to favorites in MySQL:', err);
         res.status(500).send('Internal Server Error');
       } else {
-        res.status(200).send('Produk berhasil ditambahkan ke favorit.');
+        const response = {
+          userId: userId,
+          productId: productId,
+          message: 'Produk berhasil ditambahkan ke favorit.'
+        };
+        res.status(200).json(response);
       }
     });
 });
@@ -48,7 +66,12 @@ router.post('/del-fav/:userId', (req, res) => {
         console.error('Error adding product to favorites in MySQL:', err);
         res.status(500).send('Internal Server Error');
       } else {
-        res.status(200).send('Produk berhasil dihapus dari favorit.');
+        const response = {
+          userId: userId,
+          productId: productId,
+          message: 'Produk berhasil dihapus dari favorit.'
+        };
+        res.status(200).json(response);
       }
     });
 });
@@ -56,7 +79,7 @@ router.post('/del-fav/:userId', (req, res) => {
 router.get('/favorites/:userId', (req, res) => {
     const userId = req.params.userId;
 
-    const sql = `SELECT products.*
+    const sql = `SELECT products.product_id, products.name, products.imageurl
       FROM favorites
       JOIN products ON favorites.product_id = products.product_id
       WHERE favorites.user_id = ?`;
@@ -64,7 +87,7 @@ router.get('/favorites/:userId', (req, res) => {
     db.query(sql, [userId], (err, results) => {
       if (err) {
         console.error('Error fetching favorite products from MySQL:', err);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send('Gagal menambahkan ke favorit');
       } else {
         res.status(200).json(results);
       }
